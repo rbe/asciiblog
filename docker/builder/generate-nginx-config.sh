14ae9ff4d868959983ce67362e8039f42ad9bb10
Ralf Bensmann <ralf@art-of-coding.eu>
2022-12-20 16:55:03 +0100

    nginx Konfiguration

--- docker/builder/generate-nginx-config.sh
+++ docker/builder/generate-nginx-config.sh
@@ -3,10 +3,19 @@
 set -o nounset
 set -o errexit
 
+. /mnt/content/site.env
+
 nginx_conf="/mnt/docker/webserver/urls.nginx"
 :>"${nginx_conf}"
 
-# nginx: Caching
+echo "try_files \$uri \$uri/" >>${nginx_conf}
+for g in $(find /mnt/content/blog/article -mindepth 1 -maxdepth 1 -type d -printf "%f\n")
+do
+    echo " /article/${g}\$uri" >>${nginx_conf}
+done
+echo " /book\$uri" >>${nginx_conf}
+echo " =404;" >>${nginx_conf}
+
 cat <<EOF >>"${nginx_conf}"
 location ~* .(eot|otf|woff|woff2|ttf|ico)$ {
     expires modified +1y;
@@ -22,22 +31,33 @@ location ~* .(css|js)$ {
 }
 EOF
 
-# nginx: Content-Security-Policy
-echo "add_header Content-Security-Policy \"default-src 'self'; child-src 'self' blob:\";" >>"${nginx_conf}"
+#add_header Link '<https://${SITE_BASE_URL}/assets/fontawesome/css/site.css>; rel="preconnect"';
+cat <<EOF >>"${nginx_conf}"
+add_header Strict-Transport-Security "max-age=31536000; includeSubDomains;";
+add_header Content-Security-Policy "default-src 'self'; child-src 'self' blob:;";
+#add_header Permissions-Policy: "";
+add_header Referrer-Policy "strict-origin-when-cross-origin";
+add_header X-Content-Type-Options "nosniff";
+add_header X-XSS-Protection "1; mode=block;";
+add_header X-Frame-Options "SAMEORIGIN";
+EOF
 
-# nginx: sub_filter
 cat <<EOF >>"${nginx_conf}"
 sub_filter '<link rel="stylesheet" href="./asciidoctor.css">' '';
 sub_filter '<link rel="stylesheet" href="./font-awesome.css">' '';
 EOF
 
-# nginx: try_files
-echo "try_files \$uri \$uri/" >>${nginx_conf}
-for g in $(find /mnt/content/blog/article -mindepth 1 -maxdepth 1 -type d -printf "%f\n")
-do
-    echo " /article/${g}\$uri" >>${nginx_conf}
-done
-echo " /book\$uri" >>${nginx_conf}
-echo " =404;" >>${nginx_conf}
+cat <<EOF >>"${nginx_conf}"
+gzip on;
+gzip_types text/plain
+           font/woff2
+           text/css
+           text/javascript application/javascript application/x-javascript
+           application/json
+           text/xml application/xml application/xhtml+xml application/xml+rss;
+gzip_proxied no-cache no-store no_last_modified private expired auth;
+gzip_min_length 256;
+gzip_comp_level 6;
+EOF
 
 exit 0
