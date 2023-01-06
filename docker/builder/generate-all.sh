a34ffa509e295683d2de72fc40f5e6212ba68593
Ralf Bensmann <ralf@art-of-coding.eu>
2023-01-06 08:43:14 +0100

    Generate sitemap, cleanup

--- docker/builder/generate-all.sh
+++ docker/builder/generate-all.sh
@@ -6,25 +6,36 @@ set -o errexit
 echo "*"
 echo "* INFO"
 echo "*"
+echo "asciidoctor"
 asciidoctor -v
 echo ""
+echo "node"
 node -v
 echo ""
+echo "npm"
 npm -v
 echo ""
+echo "ImageMagick convert"
 convert -version
+echo ""
 
+echo -n "Cleaning up..."
+find /mnt/publish -print0 | xargs -r -0 rm -rf
+echo "done"
+
+# Index
+generate-blog-index.sh
 # Blog: generate all groups of articles
 generate-blog.sh
-generate-blog-index.sh
 # Books: generate all books
 generate-book.sh
-generate-book-index.sh
-# nginx: try_files
-generate-nginx-config.sh
 # Root: generate content
 generate-root.sh
-# Publish
+# Sitemap
+generate-sitemap.sh
+# Publish everything to /mnt/publish
 publish.sh
+# Cleanup unneeded files
+cleanup-publish.sh
 
 exit 0
